2f51e1bd4bca993b3c39b494ef3abfdc6ed8a0ae
NDavis72516 <44639997+NDavis72516@users.noreply.github.com>
2019-03-06 12:29:45 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,11 @@
+#read in full data set and extract data for only 1/2/2007 and 2/2/2007
+house<-read.table("C:/Users/davis450/Desktop/Coursera/household_power_consumption.txt", header = TRUE, sep=";", na.strings = "?")
+house_dates<- rbind(house[house$Date=="1/2/2007",],house[house$Date=="2/2/2007",])
+
+#converting the date to R Date format
+house_dates$Date <- as.Date(house_dates$Date, "%d/%m/%Y")
+
+#plot code for plot1
+png("plot1.png", width=480, height=480)
+hist(as.numeric(house_dates$Global_active_power), col="Red", main="Global Active Power", xlab="Global Active power (kilowatts)")
+dev.off()
