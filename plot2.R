dd942594174b96e5a00399b055fb40aa21704335
NDavis72516 <44639997+NDavis72516@users.noreply.github.com>
2019-03-06 12:29:30 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ house_dates<- house[house$Date %in% c("1/2/2007","2/2/2007"),]
 #converting the date to R Date format
 datetime <- strptime(paste(house_dates$Date, house_dates$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
-#plot histogram for plot2
+#plot code for plot2
 global<- as.numeric(house_dates$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, global, type="l", xlab ="", ylab="Global Active power (kilowatts)")
